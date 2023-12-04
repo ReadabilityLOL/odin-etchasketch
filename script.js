18377364fa12884704900a345f2ca11d26db45ad
Joshua Anand <125707933+ReadabilityLOL@users.noreply.github.com>
2023-12-04 17:52:22 -0500

    tried to fix grid resizing, didn't work

--- script.js
+++ script.js
@@ -1,27 +1,27 @@
 let container = document.querySelector(".container");
-
+let numsquares = 32;
 
 function grid(){
-	for(let x = 0; x<16; x++){
+	for(let x = 0; x<numsquares; x++){
 		let line = document.createElement("div");
 		line.classList.add("line");
-		for(let x = 0; x<16;x++){
+		line.style.cssText = "width:960px;height:960px;";
+		for(let x = 0; x<numsquares;x++){
 			let box = document.createElement("div");
 			let color = 255;
 			box.classList.add("box");
+			box.style.cssText=`width:960/${numsquares};height:960/${numsquares};`;
 			box.addEventListener('mouseover',()=>{
 				color-=50;
-				box.style.cssText=`background-color:rgb(${color},${color},${color});`
+				box.style.cssText=`background-color:rgb(${color},${color},${color});`;
 			});
 			line.appendChild(box);
 		}
 		container.appendChild(line);
 	}
+
 }
 
 grid();
 
-let button = document.querySelector(".reset");
-button.addEventListener('click',()=>{
-	grid();
-});
+
